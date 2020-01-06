882e7ffee85e9ef5af60c4329127d913dbddf44c
Alastair <asmith151@outlook.com>
2020-01-06 16:15:03 +0000

    Further edits to ccanalyser. Renamed the output stats. Wrapped stats dataframe in a function to generate an output that is easier to view. Now ccanalyser_dev.py will output bedfiles containing the reporters for each capture site.

--- ccanalyser_dev.py
+++ ccanalyser_dev.py
@@ -11,7 +11,7 @@ p = argparse.ArgumentParser()
 p.add_argument('-i', '--input_bam', help='BAM file to parse')
 p.add_argument('-a', '--annotations', 
                help='Tab-delimited text file containing annotation for each read in bam file')
-p.add_argument('-o', '--output', help='output file prefix')
+p.add_argument('-o', '--output', help='output file output_prefix')
 args = p.parse_args()
 
 
@@ -34,6 +34,13 @@ def get_timing(task_name=None):
             return result
         return wrapped
     return wrapper
+
+def format_dataframe_for_printing(f):
+    @wraps(f)
+    def wrapped(*args, **kwargs):
+        df = f(*args, **kwargs)
+        return '\n'.join(str(df).split('\n')[:-1])
+    return wrapped
 
 def get_prefix():
 
@@ -85,7 +92,7 @@ def merge_annotations(df, annotations):
 
 @get_timing(task_name='classifying fragments')
 def classify_fragments(df_align):
-    
+   
     df_fragments =  (df_align.sort_values(['parent_read','coordinates'])
                              .groupby('parent_read', as_index=False)
                              .agg({'slice':'nunique', 
@@ -99,31 +106,22 @@ def classify_fragments(df_align):
                                    'restriction_fragment': 'nunique',
                                    'blacklist': 'sum',
                                    'coordinates':'|'.join
-                                  }
-                                 )
-                              .assign(capture=lambda df: df['capture'] - 1,
+                                  })
+                             .assign(capture=lambda df: df['capture'] - 1,
                                      exclusion=lambda df: df['exclusion'] -1,
-                                     restriction_fragment=lambda df: df['restriction_fragment'] - 1,
-                                     invalid_reporters=lambda df: df['mapped'] - df['exclusion_count'] - df['capture_count'] - df['blacklist']
-                                     )
+                                     reporter_count=lambda df: df['mapped'] - (df['exclusion_count'] + df['capture_count'] + df['blacklist']))
+                             .assign(reporter_count=lambda df: np.where(df['capture_count'] > 0, df['reporter_count'], 0))
                              .rename(columns={'capture': 'unique_capture_sites',
                                               'exclusion': 'unique_exclusion_sites',
                                               'restriction_fragment': 'unique_restriction_fragments',
                                               'slice': 'unique_slices',
                                               'blacklist': 'blacklisted_slices'
-                                             }
-                                    )
-                  )
-
-    # Valid reporters are defined as mapped reads that are not capture/excusion/blacklist sites
-    df_fragments['valid_reporters'] = np.where((df_fragments['capture_count'] != 0) & (df_fragments['mapped'] > 1),
-                                              df_fragments['mapped'] - df_fragments['invalid_reporters'],
-                                              0)
-    
+                                             }))    
     return df_fragments
 
+@format_dataframe_for_printing
 def get_slice_stats(df):
-    stats =  df.agg({'read_name': 'nunique',
+    stats = (df.agg({'read_name': 'nunique',
                      'parent_read': 'nunique',
                      'mapped': 'sum',
                      'multimapped':'sum',
@@ -131,25 +129,40 @@ def get_slice_stats(df):
                      'capture_count': 'sum',
                      'exclusion_count': 'sum',
                      'blacklist': 'sum'})
+               .rename({'read_name': 'unique_slices',
+                        'parent_read': 'unique_fragments',
+                        'multimapped': 'multimapping_slices',
+                        'capture': 'unique_capture_sites',
+                        'capture_count': 'number_of_capture_slices',
+                        'exclusion_count': 'number_of_slices_in_exclusion_region',
+                        'blacklist': 'number_of_slices_in_blacklisted_region',
+                        })
+             )
     return stats
 
+@format_dataframe_for_printing
 def get_frag_stats(df):
-    stats = df.agg({'parent_read': 'nunique',
+    stats = (df.agg({'parent_read': 'nunique',
                     'mapped': lambda col: (col > 1).sum(),
                     'multimapped': lambda col: (col > 0).sum(),
                     'capture_count': lambda col: (col > 0).sum(),
                     'exclusion_count': lambda col: (col > 0).sum(),
                     'blacklisted_slices': lambda col: (col > 0).sum(),
-                    'valid_reporters': lambda col: (col > 0).sum(),
-                 })
+                    'reporter_count': lambda col: (col > 0).sum()})
+                .rename({'parent_read': 'unique_fragments',
+                         'multimapped': 'fragments_with_multimapping_slices',
+                         'capture_count': 'fragments_with_capture_sites',
+                         'exclusion_count': 'fragments_with_excluded_regions',
+                         'blacklisted_slices': 'fragments_with_blacklisted_regions',
+                         'reporter_count': 'fragments_with_reporter_slices'})
+            )
     return stats
 
 @get_timing(task_name='filtering slices')
-def filter_slices(df_fragments, df_slices):
-    
-    prefix = get_prefix()
-    fragstats_fn = f'{prefix}.frag.stats'
-    slicestats_fn = f'{prefix}.slice.stats'
+def filter_slices(df_fragments, df_slices, output_prefix):
+
+    fragstats_fn = f'{output_prefix}.frag.stats'
+    slicestats_fn = f'{output_prefix}.slice.stats'
 
     with open(fragstats_fn, 'w') as fragstats_out,\
          open(slicestats_fn, 'w') as slicestats_out:
@@ -159,7 +172,6 @@ def filter_slices(df_fragments, df_slices):
         fragstats_out.write(f'Unfiltered fragment stats:\n{get_frag_stats(df_fragments)}\n')
         slicestats_out.write(f'Unfiltered slice stats:\n{get_slice_stats(df_slices.reset_index())}\n')
         
-
         #Duplicate filtering
         print('Duplicate filtered fragments (looking for exact matches)')
         df_fragments.drop_duplicates(subset="coordinates", keep=False, inplace=True)
@@ -167,49 +179,57 @@ def filter_slices(df_fragments, df_slices):
         
         #Valid reporter filtering
         print('Filtered for only fragments with valid reporter slices')
-        df_fragments.query('valid_reporters > 0', inplace=True)
+        df_fragments.query('reporter_count > 0', inplace=True)
         fragstats_out.write(f'Valid reporter filtered fragment stats:\n{get_frag_stats(df_fragments)}\n')
 
         #Useful slice filtering
-        print('Filtered for useful slices (removing unmapped, excluded, blacklisted and slices not paired with a capture)')
+        print('Filtered for useful slices (removing unmapped, excluded, blacklisted, not paired with a capture and slices mapping to two restriction fragments)')
         df_slices_filt = df_slices[df_slices['parent_read'].isin(df_fragments['parent_read'])]
         df_slices_filt = df_slices_filt.query('mapped == 1 and exclusion_count == 0 and blacklist == 0')
-        slicestats_out.write(f'Useful slice stats:\n{get_slice_stats(df_slices.reset_index())}\n')
+        df_slices_filt = (df_slices_filt.sort_values('capture_count', ascending=False) #Sort by capture sites first
+                                        .drop_duplicates(subset=['parent_read', 'restriction_fragment']))  #Stops the same restriction fragment being refered to multiple times
+        slicestats_out.write(f'Useful slice stats:\n{get_slice_stats(df_slices_filt.reset_index())}\n')
         
         # Generate .tsv of useful slices
-        print(f'Writing useful slices to {prefix}.tsv')
-        df_slices_filt.to_csv(f'{prefix}.useful_slices.tsv', sep='\t')
+        print(f'Writing useful slices to {output_prefix}.tsv')
+        df_slices_filt.to_csv(f'{output_prefix}.useful_slices.tsv', sep='\t')
 
-        # Generate .bed file of reporter slices
-        print(f'Writing reporter slices to {prefix}.reporter.bed')
-        (df_slices_filt.reset_index()
-                       .query('capture_count == 0')
-                       [['chrom', 'start', 'end', 'read_name']]
-                       .to_csv(f'{prefix}.reporter.bed', sep='\t', header=False, index=False)
-        )
+        # Generate .bed file of capture and reporter slices
+        print(f'Writing reporter slices to {output_prefix}.reporter.bed')
+        df_slices_capture = (df_slices_filt.reset_index()
+                                           .query('capture_count > 0'))
+        df_slices_reporter = (df_slices_filt.reset_index()
+                                           .query('capture_count == 0'))
         
-    return df_fragments, df_slices_filt
+        df_slices_capture[['chrom', 'start', 'end', 'read_name']].to_csv(f'{output_prefix}.capture.bed', sep='\t', header=None, index=False)
+        df_slices_reporter[['chrom', 'start', 'end', 'read_name']].to_csv(f'{output_prefix}.reporter.bed', sep='\t', header=None, index=False)        
+        
+    return df_fragments, df_slices_capture, df_slices_reporter
 
+def get_reporters_by_capture_site(df_capture, df_reporter):
+    
 
+    df_capture_merged = (df_capture[['parent_read', 'capture']]
+                                   .merge(df_reporter[['parent_read', 'read_name', 'chrom', 'start', 'end']], on='parent_read'))
+                                   
+    return {capture_site: reporters for capture_site, reporters in df_capture_merged.groupby('capture')}
+
+@get_timing(task_name='analysis of bam file')
 def main():
+
+    output_prefix = get_prefix()
 
     df_alignment = parse_bam(args.input_bam)
     df_alignment = merge_annotations(df_alignment, args.annotations)
     df_fragments = classify_fragments(df_alignment)
 
-    df_fragments_filt, df_alignment_filt = filter_slices(df_fragments, df_alignment)
-    
+    df_fragments_filt, df_capture, df_reporter = filter_slices(df_fragments, df_alignment, output_prefix)
 
-
-
-
-
-    
-
-
-
-
-
+    # Output all reporter fragments grouped by capture site
+    print('Outputing reporter fragments by capture site')
+    reporters_by_site = get_reporters_by_capture_site(df_capture, df_reporter)
+    for cap_site, reporters in reporters_by_site.items():
+        reporters[['chrom', 'start', 'end', 'read_name']].to_csv(f'{output_prefix}_{cap_site}.bed', sep='\t', header=False, index=False)
 
 
 if __name__ == '__main__':
