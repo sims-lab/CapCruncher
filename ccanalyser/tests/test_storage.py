3b1117e5ebcd50b7d63ac42c79ee311b8b6e263c
alsmith <asmith151@outlook.com>
2021-03-30 11:23:52 +0100

    Added tests folder as this was removed

--- ccanalyser/tests/test_storage.py
+++ ccanalyser/tests/test_storage.py
@@ -6,7 +6,6 @@ import pytest
 import click
 from click.testing import CliRunner
 
-from ccanalyser.cli import interactions_store
 from ccanalyser.tools.storage import GenomicBinner, CoolerBinner, create_cooler_cc
 
 
@@ -17,20 +16,21 @@ dir_data = os.path.join(dir_package, "data")
 
 
 def test_make_cooler():
-    pixels = pd.read_csv(os.path.join(dir_data, "test", "RS411_0uM_1.BCL2.tsv.gz"), sep='\t')
+    pixels = pd.read_csv(os.path.join(dir_data, "test", "Slc25A37_reporter_counts.tsv.gz"), sep='\t')
     bins = pd.read_csv(
         os.path.join(dir_data, "test", "genome.digest.bed.gz"),
         sep="\t",
         names=["chrom", "start", "end", "name"],
     )
     oligos = os.path.join(dir_data, "test", "capture_oligos.bed")
-    output_prefix = "test/cooler"
-    outfile = "test/cooler.BCL2.hdf5"
+    output_prefix = os.path.join(dir_test, "test/cooler")
+    outfile = os.path.join(dir_test, "test/cooler.Slc25A37.hdf5")
 
     if os.path.exists(outfile):
         os.unlink(outfile)
 
-    create_cooler_cc(output_prefix, bins, pixels, "BCL2", oligos)
+    breakpoint()
+    create_cooler_cc(output_prefix, bins, pixels, "Slc25A37", oligos)
 
     assert os.path.exists(outfile)
 
@@ -42,8 +42,8 @@ def test_make_cooler():
 
 def test_binning():
 
-    cooler_fn = "test/cooler.BCL2.hdf5"
-    outfile = 'test/cooler.BCL2.binned.hdf5'
+    cooler_fn = "test/cooler.Slc25A37.hdf5"
+    outfile = 'test/cooler.Slc25A37.binned.hdf5'
     cb = CoolerBinner(cooler_fn, binsize=2500, n_cores=8)
     cb.to_cooler(outfile, normalise=False, scale_factor=1e6)
 
