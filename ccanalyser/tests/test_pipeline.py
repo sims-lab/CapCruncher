7e6909e13c6666dfc5688df09b43441f1468f0fd
alsmith <asmith151@outlook.com>
2021-04-07 11:53:06 +0100

    Added basic test for pipeline run

---
+++ ccanalyser/tests/test_pipeline.py
@@ -0,0 +1,56 @@
+import os
+import sys
+import subprocess
+import shutil
+import re
+
+
+
+# Pre-run setup
+dir_test = os.path.realpath(os.path.dirname(__file__))
+dir_test_run = os.path.join(dir_test, 'pipeline_test_run')
+dir_package = os.path.dirname(dir_test)
+dir_data = os.path.join(dir_package, "data")
+
+# data paths
+data_path_fq1 = os.path.join(dir_data, 'test', 'Slc25A37-test_1_1.fastq.gz')
+data_path_fq2 = os.path.join(dir_data, 'test', 'Slc25A37-test_1_2.fastq.gz')
+data_path_oligos = os.path.join(dir_data, 'test', 'mm9_capture_oligos.bed')
+data_path_genome = os.path.join(dir_data, 'test', 'data_for_pipeline_run', 'chr14.fa.gz')
+data_path_chromsizes = os.path.join(dir_data, 'test', 'data_for_pipeline_run', 'chr14.fa.fai')
+data_path_bowtie2_indicies = os.path.join(dir_data, 'test', 'data_for_pipeline_run', 'chr14_bowtie2_indicies')
+data_path_config = os.path.join(dir_data, 'test', 'data_for_pipeline_run', 'config.yml')
+
+# Set-up
+os.chdir(dir_test_run)
+cwd = os.getcwd()
+
+## Copy fastq file
+shutil.copy(data_path_fq1, cwd)
+shutil.copy(data_path_fq2, cwd)
+
+## Read config and replace with correct paths
+replacements = {'PATH_TO_CAPTURE_OLIGOS': data_path_oligos,
+                'PATH_TO_FASTA': data_path_genome,
+                'PATH_TO_INDICIES': data_path_bowtie2_indicies,
+                'PATH_TO_FAI': data_path_chromsizes,
+                }
+            
+with open(data_path_config, 'r') as config:
+    with open('config.yml', 'w') as writer:
+        for line in config:
+            for key in replacements:
+                if key in line:
+                    line = line.replace(key, replacements[key])
+
+            writer.write(line.strip() + '\n')
+
+
+
+def test_pipeline_fastq_preprocessing():
+
+    cmd = 'ccanalyser pipeline make fastq_preprocessing'
+    return_code = subprocess.call(cmd.split())
+    assert return_code == 0
+
+    
