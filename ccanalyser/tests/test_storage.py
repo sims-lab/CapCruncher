61660f5633f5163756a2e66755f1d038892bfdab
alsmith <asmith151@outlook.com>
2021-05-24 10:57:46 +0100

    Added genomic binner usage tests

--- ccanalyser/tests/test_storage.py
+++ ccanalyser/tests/test_storage.py
@@ -25,6 +25,9 @@ def cleanup():
     # Remove previous test files
     for fn in glob.glob(os.path.join(dir_test, "test", "*.hdf5")):
         os.unlink(fn)
+    
+    os.unlink(os.path.join(dir_test, "test", 'genome_binner.pkl'))
+
 
 
 def test_make_cooler():
@@ -61,3 +64,41 @@ def test_binning():
     cb = CoolerBinner(cooler_fn, binsize=2500, n_cores=8)
     cb.to_cooler(outfile, normalise=False, scale_factor=1e6)
     assert os.path.exists(f'{outfile}.Slc25A37.2500.hdf5')
+
+
+def test_make_bin_conversion():
+
+    from ccanalyser.tools.storage import GenomicBinner
+    import pickle
+
+    frags = pd.read_csv(
+        os.path.join(dir_data, "test", "genome.digest.bed.gz"),
+        sep="\t",
+        names=["chrom", "start", "end", "name"],
+    )
+
+    gb = GenomicBinner(
+            chromsizes=os.path.join(dir_data, "test", "genome.fa.fai"), fragments=frags, binsize=100000)
+    
+    gb.bin_conversion_table
+
+    with open(os.path.join(dir_test, "test", "genome_binner.pkl"), "wb") as w:
+        pickle.dump(gb, w)
+
+
+def test_binning_with_conversion_table():
+
+    import pickle
+
+    cooler_fn = os.path.join(dir_test, "test", "cooler.Slc25A37.hdf5")
+    outfile = os.path.join(dir_test, "test", "cooler.gb.binned")
+
+    with open(os.path.join(dir_test, "test", "genome_binner.pkl"), "rb") as r:
+        gb = pickle.load(r)
+
+    breakpoint()
+    cb = CoolerBinner(cooler_fn, binner=gb)
+    cb.to_cooler(outfile, normalise=False, scale_factor=1e6)
+    assert os.path.exists(f'{outfile}.Slc25A37.1000000.hdf5')
+
+
