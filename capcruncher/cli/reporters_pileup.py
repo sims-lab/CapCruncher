3e73515b4b25145e89d80713e97862d65221fdbe
asmith <asmith151@outlook.com>
2021-10-12 14:37:18 +0100

    Added normalisation into reporters_pileup

--- capcruncher/cli/reporters_pileup.py
+++ capcruncher/cli/reporters_pileup.py
@@ -1,16 +1,20 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+import os
+from typing import Literal, Union
+
 import cooler
 from capcruncher.tools.pileup import CoolerBedGraph, CoolerBedGraphWindowed
 from capcruncher.tools.storage import CoolerBinner
-import os
+
 
 def bedgraph(
     cooler_fn: os.PathLike,
-    capture_names: list = None,
+    viewpoint_names: list = None,
     output_prefix: os.PathLike = "",
-    normalise: bool = False,
+    normalisation: Literal["raw", "n_cis", "region"] = "raw",
+    normalisation_regions: os.PathLike = None,
     binsize: int = 0,
     gzip: bool = True,
     scale_factor: int = 1e6,
@@ -19,18 +23,18 @@ def bedgraph(
     """
     Extracts reporters from a capture experiment and generates a bedgraph file.
 
-    Identifies reporters for a single probe (if a probe name is supplied) or all capture 
-    probes present in a capture experiment HDF5 file. 
-    
+    Identifies reporters for a single probe (if a probe name is supplied) or all capture
+    probes present in a capture experiment HDF5 file.
+
     The bedgraph generated can be normalised by the number of cis interactions for
     inter experiment comparisons and/or binned into even genomic windows.
 
     \f
     Args:
      cooler_fn (os.PathLike): Path to hdf5 file containing cooler groups.
-     capture_names (list, optional): Name of capture probe to extract.
-                                     If None, will process all probes present in the file.
-                                     Defaults to None.
+     viewpoint_names (list, optional): Name of viewpoints to extract. 
+                                       If None, will process all probes present in the file.
+                                       Defaults to None.
      output_prefix (os.PathLike, optional): Output file prefix for bedgraph. Defaults to "".
      normalise (bool, optional): Normalise counts using the number of cis interactions. Defaults to False.
      binsize (int, optional): Genomic binsize to use for generating bedgraph. No binning performed if less than 0. Defaults to 0.
@@ -39,39 +43,42 @@ def bedgraph(
      sparse (bool, optional): Produce bedgraph containing just positive bins (True) or all bins (False). Defaults to True.
     """
 
-    if (
-        not capture_names
-    ):  # If no probe names provided extract all restriction fragment cooler objects
-        capture_names = [
-            c.strip("/")
-            for c in cooler.fileops.list_coolers(cooler_fn)
-            if not "resolutions" in c
-        ]
+    viewpoint_names = viewpoint_names or [
+        v.strip("/")
+        for v in cooler.fileops.list_coolers(cooler_fn)
+        if not "resolutions" in v
+    ]
 
-    for ii, capture_name in enumerate(capture_names):
+    bin_bedgraph = True if binsize > 0 else False
 
-        if binsize == 0:
+    for ii, viewpoint_name in enumerate(viewpoint_names):
+
+        if not bin_bedgraph:
             bedgraph = CoolerBedGraph(
-                cooler_fn=f"{cooler_fn}::{capture_name}", sparse=sparse
+                cooler_fn=f"{cooler_fn}::{viewpoint_name}", sparse=sparse
             )
 
-        elif (
-            ii == 0 and binsize > 0
-        ):  # Only want to bin once and then re-use this for the rest
+        elif ii == 0 and bin_bedgraph:
+            # Only want to bin once and then re-use this for the rest
             binner = CoolerBinner(
-                cooler_fn=f"{cooler_fn}::{capture_name}", binsize=binsize
+                cooler_fn=f"{cooler_fn}::{viewpoint_name}", binsize=binsize
             )
             bedgraph = CoolerBedGraphWindowed(
-                cooler_fn=f"{cooler_fn}::{capture_name}", binner=binner, sparse=sparse
+                cooler_fn=f"{cooler_fn}::{viewpoint_name}", binner=binner, sparse=sparse
             )
 
-        elif binsize > 0:
+        else:
             bedgraph = CoolerBedGraphWindowed(
-                cooler_fn=f"{cooler_fn}::{capture_name}", binner=binner, sparse=sparse
+                cooler_fn=f"{cooler_fn}::{viewpoint_name}", binner=binner, sparse=sparse
+            )
+
+        if normalisation in ["n_cis", "region"]:
+            bedgraph.normalise_bedgraph(
+                scale_factor=scale_factor,
+                norm_method=normalisation,
+                norm_region=normalisation_regions,
             )
 
         bedgraph.to_file(
-            f'{output_prefix}.{capture_name}.bedgraph{".gz" if gzip else ""}',
-            normalise=normalise,
-            scale_factor=scale_factor,
+            f'{output_prefix}.{viewpoint_name}.bedgraph{".gz" if gzip else ""}',
         )
