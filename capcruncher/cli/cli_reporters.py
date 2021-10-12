32973e8f53f88e9c192cc7ceae6534de5492f110
asmith <asmith151@outlook.com>
2021-10-12 14:46:16 +0100

    Added normalisation methods and regions to the cli

--- capcruncher/cli/cli_reporters.py
+++ capcruncher/cli/cli_reporters.py
@@ -1,63 +1,104 @@
-import click 
+import click
+
 
 @click.group()
 def cli():
-    """Contains methods for interaction counting, storing, bedgraph generation, comparisons.
-    """
+    """Contains methods for interaction counting, storing, bedgraph generation, comparisons."""
 
 
 @cli.command()
-@click.argument('union_bedgraph')
-@click.option('-n', '--capture_name', help='Name of capture probe, must be present in viewpoint file.', required=True)
-@click.option('-c', '--capture_viewpoints', help='Path to capture viewpoints bed file', required=True)
-@click.option('-o', '--output_prefix', help='Output prefix for pairwise statistical comparisons', default='out')
-@click.option('--design_matrix', help='Path tsv file containing sample annotations (N_SAMPLES * N_INFO_COLUMNS)', default=None)
-@click.option('--grouping_col', help='Column to use for grouping replicates', default='condition')
-@click.option('--threshold_count', help='Minimum count required to be considered for analysis', default=20, type=click.FLOAT)
-@click.option('--threshold_q', help='Upper threshold of q-value required for output.', default=0.05, type=click.FLOAT)
-@click.option('--threshold_mean', help='Minimum mean count required for output.', default=0, type=click.FLOAT)
+@click.argument("union_bedgraph")
+@click.option(
+    "-n",
+    "--capture_name",
+    help="Name of capture probe, must be present in viewpoint file.",
+    required=True,
+)
+@click.option(
+    "-c",
+    "--capture_viewpoints",
+    help="Path to capture viewpoints bed file",
+    required=True,
+)
+@click.option(
+    "-o",
+    "--output_prefix",
+    help="Output prefix for pairwise statistical comparisons",
+    default="out",
+)
+@click.option(
+    "--design_matrix",
+    help="Path tsv file containing sample annotations (N_SAMPLES * N_INFO_COLUMNS)",
+    default=None,
+)
+@click.option(
+    "--grouping_col", help="Column to use for grouping replicates", default="condition"
+)
+@click.option(
+    "--threshold_count",
+    help="Minimum count required to be considered for analysis",
+    default=20,
+    type=click.FLOAT,
+)
+@click.option(
+    "--threshold_q",
+    help="Upper threshold of q-value required for output.",
+    default=0.05,
+    type=click.FLOAT,
+)
+@click.option(
+    "--threshold_mean",
+    help="Minimum mean count required for output.",
+    default=0,
+    type=click.FLOAT,
+)
 def differential(*args, **kwargs):
     """
     Identifies differential interactions between conditions.
 
     Parses a union bedgraph containg reporter counts from at least two conditions with
     two or more replicates for a single capture probe and outputs differential interaction
-    results. Following filtering to ensure that the number of interactions is above the required 
-    threshold (--threshold_count), diffxpy is used to run a wald test after 
+    results. Following filtering to ensure that the number of interactions is above the required
+    threshold (--threshold_count), diffxpy is used to run a wald test after
     fitting a negative binomial model to the interaction counts.The options to filter
-    results can be filtered by a minimum mean value (threshold_mean) and/or 
+    results can be filtered by a minimum mean value (threshold_mean) and/or
     maximum q-value (threshold-q) are also provided.
 
     Notes:
-        
-     Currently both the capture viewpoints and the name of the probe being analysed must 
+
+     Currently both the capture viewpoints and the name of the probe being analysed must
      be provided in order to correctly extract cis interactions.
- 
-     If a N_SAMPLE * METADATA design matrix has not been supplied, the script 
-     assumes that the standard replicate naming structure has been followed 
-     i.e. SAMPLE_CONDITION_REPLICATE_(1|2).fastq.gz.    
+
+     If a N_SAMPLE * METADATA design matrix has not been supplied, the script
+     assumes that the standard replicate naming structure has been followed
+     i.e. SAMPLE_CONDITION_REPLICATE_(1|2).fastq.gz.
     """
 
     from capcruncher.cli.reporters_differential import differential
-    differential(*args, **kwargs) 
 
-
+    differential(*args, **kwargs)
 
 
 @cli.command()
 @click.argument("cooler_fn")
 @click.option(
     "-n",
-    "--capture_names",
-    help="Capture to extract and convert to bedgraph, if not provided will transform all.",
+    "--viewpoint_names",
+    help="Viewpoint to extract and convert to bedgraph, if not provided will transform all.",
     multiple=True,
 )
 @click.option("-o", "--output_prefix", help="Output prefix for bedgraphs")
 @click.option(
-    "--normalise",
-    help="Normalised bedgraph (Correct for number of cis reads)",
-    default=False,
-    is_flag=True,
+    "--normalisation",
+    help="Method to use interaction normalisation",
+    default="raw",
+    type=click.Choice(["raw", "n_cis" "region"]),
+)
+@click.option(
+    "--normalisation-regions",
+    help="Regions to use for interaction normalisation. The --normalisation method MUST be 'region'",
+    default=None,
+    type=click.STRING,
 )
 @click.option(
     "--binsize",
@@ -80,14 +121,15 @@ def pileup(*args, **kwargs):
     """
     Extracts reporters from a capture experiment and generates a bedgraph file.
 
-    Identifies reporters for a single probe (if a probe name is supplied) or all capture 
-    probes present in a capture experiment HDF5 file. 
-    
+    Identifies reporters for a single probe (if a probe name is supplied) or all capture
+    probes present in a capture experiment HDF5 file.
+
     The bedgraph generated can be normalised by the number of cis interactions for
     inter experiment comparisons and/or binned into even genomic windows.
     """
 
     from capcruncher.cli.reporters_pileup import bedgraph
+
     bedgraph(*args, **kwargs)
 
 
@@ -127,13 +169,14 @@ def count(*args, **kwargs):
     """
     Determines the number of captured restriction fragment interactions genome wide.
 
-    Parses a reporter slices tsv and counts the number of unique restriction fragment 
+    Parses a reporter slices tsv and counts the number of unique restriction fragment
     interaction combinations that occur within each fragment.
-    
-    Options to ignore unwanted counts e.g. excluded regions or capture fragments are provided. 
+
+    Options to ignore unwanted counts e.g. excluded regions or capture fragments are provided.
     In addition the number of reporter fragments can be subsampled if required.
     """
     from capcruncher.cli.reporters_count import count
+
     count(*args, **kwargs)
 
 
@@ -142,14 +185,15 @@ def store():
     """
     Store reporter counts.
 
-    These commands store and manipulate reporter restriction fragment interaction 
+    These commands store and manipulate reporter restriction fragment interaction
     counts as cooler formated groups in HDF5 files.
 
-    See subcommands for details. 
+    See subcommands for details.
 
     """
 
-@store.command(name='fragments')
+
+@store.command(name="fragments")
 @click.argument("counts", required=True)
 @click.option(
     "-f",
@@ -190,14 +234,16 @@ def store_fragments(*args, **kwargs):
     """
     Stores restriction fragment interaction combinations at the restriction fragment level.
 
-    Parses reporter restriction fragment interaction counts produced by 
-    "capcruncher reporters count" and gerates a cooler formatted group in an HDF5 File. 
+    Parses reporter restriction fragment interaction counts produced by
+    "capcruncher reporters count" and gerates a cooler formatted group in an HDF5 File.
     See `https://cooler.readthedocs.io/en/latest/` for further details.
     """
     from capcruncher.cli.reporters_store import fragments
+
     fragments(*args, **kwargs)
 
-@store.command(name='bins')
+
+@store.command(name="bins")
 @click.argument("cooler_fn", required=True)
 @click.option(
     "-b",
@@ -245,25 +291,25 @@ def store_bins(*args, **kwargs):
     """
     Convert a cooler group containing restriction fragments to constant genomic windows
 
-    Parses a cooler group and aggregates restriction fragment interaction counts into 
-    genomic bins of a specified size. If the normalise option is selected,  
+    Parses a cooler group and aggregates restriction fragment interaction counts into
+    genomic bins of a specified size. If the normalise option is selected,
     columns containing normalised counts are added to the pixels table of the output
     """
     from capcruncher.cli.reporters_store import bins
+
     bins(*args, **kwargs)
 
-@store.command(name='merge')
+
+@store.command(name="merge")
 @click.argument("coolers", required=True, nargs=-1)
 @click.option("-o", "--output", help="Output file name")
 def store_merge(*args, **kwargs):
     """
     Merges capcruncher cooler files together.
-    
+
     Produces a unified cooler with both restriction fragment and genomic bins whilst
     reducing the storage space required by hard linking the "bins" tables to prevent duplication.
     """
     from capcruncher.cli.reporters_store import merge
+
     merge(*args, **kwargs)
-
-
-
