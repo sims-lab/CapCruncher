c1017958e69253c79bbc702ac31d49a33731f989
asmith <asmith151@outlook.com>
2021-10-29 11:58:57 +0100

    Support for unspecified viewpoints

--- capcruncher/cli/alignments_deduplicate.py
+++ capcruncher/cli/alignments_deduplicate.py
@@ -1,4 +1,4 @@
-from typing import Iterable
+from typing import Iterable, List, Literal
 import pandas as pd
 
 import click
@@ -8,40 +8,24 @@ from capcruncher.utils import hash_column, load_json
 import ujson
 import os
 import numpy as np
+import dask.dataframe as dd
 
-def identify(
-    fragments_fn: os.PathLike,
-    output: os.PathLike = "duplicated_ids.json",
-    buffer: int = 1e6,
-    read_type: str = "flashed",
+
+def extract_start_and_end_slice_coords_pe(df):
+    coords = df["coordinates"].str.extract(
+        r"^chr(?P<chrom1>.*?):(?P<start>\d+).*\|chr(?P<chrom2>.*?):\d+-(?P<end>\d+)"
+    )
+    coords = coords["chrom1"].str.cat(coords.iloc[:, 1:])
+    coords.name = "coordinates_pe"
+    return coords
+
+
+def identify_deduplicates_from_tsv(
+    fragments: pd.DataFrame, read_type: Literal["flashed", "pe"], buffer: float = 1e6
 ):
-    """
-    Identifies aligned fragments with duplicate coordinates.
 
-    Parses a tsv file containing filtered aligned fragments and generates a dictionary containing
-    the hashed parental read id and hashed genomic coordinates of all slices. Duplicated fragments
-    are implicitly removed if they share the same genomic coordinate hash.
-
-    For non-combined reads (pe) a genomic coordinate hash is generated from the start of the first slice and
-    the end of the last slice. This is due to the decreased confidence in the quality of the centre of the fragment.
-    The coordinate hash for combined reads (flashed) is generated directly from the fragment coordinates. Only
-    fragments with the exact coordinates and slice order will be considered to be duplicates.
-
-    Identified duplicate fragments are output in json format to be used by the "remove" subcommand.
-
-    \f
-    Args:
-     fragments_fn (os.PathLike): Input fragments.tsv file to process.
-     output (os.PathLike, optional): Output path to output duplicated parental read ids. Defaults to "duplicated_ids.json".
-     buffer (int, optional): Number of fragments to process in memory. Defaults to 1e6.
-     read_type (str, optional): Process combined(flashed) or non-combined reads (pe).
-                                Due to the low confidence in the quaility of pe reads, duplicates are identified by
-                                removing any fragments with matching start and end coordinates.
-                                Defaults to "flashed".
-    """
-
-    fragments = pd.read_csv(
-        fragments_fn,
+    df_fragments = pd.read_csv(
+        fragments,
         sep="\t",
         chunksize=buffer,
         usecols=["parent_read", "coordinates"],
@@ -77,6 +61,87 @@ def identify(
             else:
                 duplicated_fragments.add(id)
 
+    return duplicated_fragments
+
+
+def identify_duplicates_from_hdf5(
+    fragments: list, viewpoint: str, read_type: Literal["flashed", "pe"]
+):
+
+    df_fragments_coords = dd.read_hdf(fragments, key=f"{viewpoint}/fragments", columns=["parent_id", "coordinates"])
+
+    if read_type == "flashed":
+
+        return set(
+            df_fragments_coords
+            .drop_duplicates(subset="coordinates")
+            ["parent_id"]
+        )
+
+    elif read_type == "pe":
+
+        return set(
+            df_fragments_coords.map_partitions(
+                lambda df: df.join(extract_start_and_end_slice_coords_pe(df))[
+                    ["parent_id", "coordinates_pe"]
+                ]
+            )
+            .drop_duplicates(subset="coordinates_pe")
+            ["parent_id"]
+        )
+
+
+def identify(
+    fragments: os.PathLike,
+    input_type: str = "hdf5",
+    output: os.PathLike = "duplicated_ids.json",
+    viewpoint: str = "",
+    buffer: int = 1e6,
+    read_type: str = "flashed",
+):
+    """
+    Identifies aligned fragments with duplicate coordinates.
+
+    Parses a tsv file containing filtered aligned fragments and generates a dictionary containing
+    the hashed parental read id and hashed genomic coordinates of all slices. Duplicated fragments
+    are implicitly removed if they share the same genomic coordinate hash.
+
+    For non-combined reads (pe) a genomic coordinate hash is generated from the start of the first slice and
+    the end of the last slice. This is due to the decreased confidence in the quality of the centre of the fragment.
+    The coordinate hash for combined reads (flashed) is generated directly from the fragment coordinates. Only
+    fragments with the exact coordinates and slice order will be considered to be duplicates.
+
+    Identified duplicate fragments are output in json format to be used by the "remove" subcommand.
+
+    \f
+    Args:
+     fragments_fn (os.PathLike): Input fragments.tsv file to process.
+     output (os.PathLike, optional): Output path to output duplicated parental read ids. Defaults to "duplicated_ids.json".
+     buffer (int, optional): Number of fragments to process in memory. Defaults to 1e6.
+     read_type (str, optional): Process combined(flashed) or non-combined reads (pe).
+                                Due to the low confidence in the quaility of pe reads, duplicates are identified by
+                                removing any fragments with matching start and end coordinates.
+                                Defaults to "flashed".
+    """
+
+    if input_type == "tsv":
+        if len(fragments) > 1:
+            raise NotImplementedError("Currently just supports a single tsv input")
+        else:
+            duplicated_fragments = identify_deduplicates_from_tsv(fragments[0], read_type=read_type, buffer=buffer)
+
+    elif input_type == "hdf5":
+
+        if viewpoint == "":
+            with pd.HDFStore(fragments[0]) as store:
+                viewpoints = [k.split("/")[1] for k in store.keys()]
+        else:
+            # Need a fake list to iterate
+            viewpoints = [viewpoint, ]
+            
+        for viewpoint in viewpoints:
+            duplicated_fragments = identify_duplicates_from_hdf5(fragments, viewpoint=viewpoint, read_type=read_type)
+
     with xopen.xopen(output, "w") as w:
         ujson.dump(dict.fromkeys(duplicated_fragments), w)
 
@@ -89,6 +154,7 @@ def remove(
     sample_name: str = "",
     read_type: str = "",
     stats_prefix: os.PathLike = "",
+    input_type: str = "hdf5"
 ):
     """
     Removes duplicated aligned fragments.
@@ -109,6 +175,7 @@ def remove(
      read_type (str, optional): Process combined(flashed) or non-combined reads (pe) used for statistics. Defaults to "".
      stats_prefix (os.PathLike, optional): Output path for deduplication statistics. Defaults to "".
     """
+
 
     # Remove output if it exist as will need to append to file.
     if os.path.exists(output):
