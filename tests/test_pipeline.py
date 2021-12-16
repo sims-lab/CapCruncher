ac890bcae3440a8f2e2806da09286ef8f8d461ed
alsmith <asmith151@outlook.com>
2021-12-16 14:29:14 +0000

    Using fixtures for pipeline test

---
+++ tests/test_pipeline.py
@@ -0,0 +1,148 @@
+import os
+import sys
+import subprocess
+import shutil
+import glob
+import pytest
+import tempfile
+
+@pytest.fixture(scope="session")
+def data_path():
+    fn = os.path.realpath(__file__)
+    dirname = os.path.dirname(fn)
+    data_dir = os.path.join(dirname, "data", "data_for_pipeline_run")
+    return data_dir
+
+@pytest.fixture(scope="session")
+def genome(data_path):
+    return os.path.join(data_path, 'chr14.fa.gz')
+
+@pytest.fixture(scope="session")
+def indicies(data_path, genome):
+
+    indicies = os.path.join(data_path, 'chr14_bowtie2_indicies')
+    if not os.path.exists(indicies):
+        os.mkdir(indicies)
+        cmd = f"bowtie2-build {genome} {indicies}/bt2 --threads 8"
+        subprocess.run(cmd.split())
+    
+    return os.path.join(indicies, "bt2")
+
+@pytest.fixture(scope="session")
+def config_yaml(data_path):
+    current_dir = os.path.dirname(os.path.realpath(__file__))
+    repo_dir = os.path.dirname(current_dir)
+    config = os.path.join(repo_dir, "config.yml")
+    return config
+
+
+@pytest.fixture(scope="session")
+def run_directory(tmpdir_factory):
+    fn = tmpdir_factory.mktemp("data")
+    return fn
+
+    
+@pytest.fixture()
+def setup_pipeline_run(data_path, run_directory, genome, indicies, config_yaml):
+
+    oligos = os.path.join(data_path, 'mm9_capture_oligos_Slc25A37.bed')
+    chromsizes = os.path.join(data_path, 'chr14.fa.fai')
+    fastq = glob.glob(os.path.join(data_path, "*.fastq*"))
+
+    os.chdir(run_directory)
+
+    for fn in fastq:
+        shutil.copy(fn, ".")   
+
+    ## Read config and replace with correct paths
+    replacements = {'PATH_TO_VIEWPOINTS': oligos,
+                    'PATH_TO_GENOME_FASTA': genome,
+                    'PATH_TO_ALIGNER_INDICIES': indicies,
+                    'PATH_TO_CHROMOSOME_SIZES': chromsizes,
+                    'PATH_TO_HUB_DIRECTORY': os.path.join(run_directory, "hub_directory"),
+                    'PATH_TO_PLOTTING_COORDINATES': os.path.join(data_path, 'plot_coords.bed'),
+                    'PATH_TO_TSV_FORMATTED_DESIGN_MATRIX': os.path.join(data_path, 'design_matrix.tsv'),
+                    'PATH_TO_GENES_IN_BED12_FORMAT': os.path.join(data_path, 'mm9_chr14_genes.bed'),
+                    'HUB_NAME': 'CAPCRUNCHER_TEST_HUB',
+                    'REGIONS_FOR_NORM': os.path.join(data_path, 'regions_for_norm.bed'),
+                    }
+                
+    with open(config_yaml, 'r') as config:
+        with open('config.yml', 'w') as writer:
+            for line in config:
+                for key in replacements:
+                    if key in line:
+                        line = line.replace(key, replacements[key])
+
+                writer.write(line)
+    
+    yield
+
+# def test_pipeline_handles_no_drmaa(setup_pipeline_run):
+#     cmd = f'capcruncher pipeline make annotate_sort_viewpoints'
+#     completed = subprocess.run(cmd.split())
+#     assert completed.returncode == 0
+
+def test_pipeline_fastq_preprocessing(setup_pipeline_run):
+
+    cmd = f'capcruncher pipeline make fastq_preprocessing --local -p 4'
+    completed = subprocess.run(cmd.split())
+    assert completed.returncode == 0
+    assert len(glob.glob('capcruncher_preprocessing/digested/*.fastq.gz')) == 12
+
+def test_pipeline_pre_annotation(setup_pipeline_run):
+
+    cmd = f'capcruncher pipeline make pre_annotation --local -p 4'
+    completed = subprocess.run(cmd.split())
+    assert completed.returncode == 0
+
+def test_pipeline_post_annotation(setup_pipeline_run):
+    cmd = f'capcruncher pipeline make post_annotation --local -p 4'
+    completed = subprocess.run(cmd.split())
+    assert completed.returncode == 0
+
+def test_pipeline_post_capcruncher_analysis(setup_pipeline_run):
+    cmd = f'capcruncher pipeline make post_capcruncher_analysis --local -p 4'
+    completed = subprocess.run(cmd.split())
+    breakpoint()
+    assert completed.returncode == 0
+
+def test_pipeline_all(setup_pipeline_run):
+
+    cmd = f'capcruncher pipeline make full --local -p 4'
+    completed = subprocess.run(cmd.split())
+    breakpoint()
+    assert completed.returncode == 0
+
+def test_stats_exist():
+
+    assert os.path.exists('capcruncher_statistics/capcruncher_statistics.html')
+
+def test_bigwigs_exist():
+    assert len(glob.glob('capcruncher_analysis/bigwigs/*.bigWig')) == 16
+
+def test_hub_exists():
+    assert os.path.exists('capturec_test.hub.txt')
+
+def test_plot_template_exists():
+    try:
+        import coolbox
+        assert os.path.exists("capcruncher_plots/templates/Slc25A37.pileup.yml")
+    except ImportError:
+        pass
+
+def test_plot_exists():
+    try:
+        import coolbox
+        assert os.path.exists("capcruncher_plots/Slc25A37_chr14:69878554-69933221.svg")
+    except ImportError:
+        pass
+
+
+
+
+
+
+
+
+
