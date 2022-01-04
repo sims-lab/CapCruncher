4d9bf99f3fca0d68745f1dede3202daeb63d8ec0
alsmith <asmith151@outlook.com>
2022-01-04 17:02:28 +0000

    Fixed bug removing duplicates. Explicitly set categorical cols

--- capcruncher/tools/deduplicate.py
+++ capcruncher/tools/deduplicate.py
@@ -411,18 +411,21 @@ def remove_duplicates_from_parquet(
 
     duplicates = tuple(duplicated_ids.values)
 
-    n_slices_total = dd.read_parquet(slices, columns=["parent_id"]).shape[0].compute()
+    n_slices_total = dd.read_parquet(slices, columns=["parent_id"], engine="pyarrow").shape[0].compute()
 
+    logging.info("Loading and filtering slices")
     ddf = dd.read_parquet(
         slices,
         filters=[("parent_id", "not in", duplicates)],
         engine="pyarrow-dataset",
     )
 
-    ddf = ddf.categorize()
+    ddf = ddf.categorize(columns=["capture", "viewpoint", "exclusion", "chrom"], index=False)
+
+    logging.info("Writing unique slices")
     ddf.to_parquet(output, compression="snappy", engine="pyarrow")
     
-    n_slices_unique = dd.read_parquet(output, columns=["parent_id"]).shape[0].compute()
+    n_slices_unique = dd.read_parquet(output, columns=["parent_id"], engine="pyarrow").shape[0].compute()
     return (n_slices_total, n_slices_unique)
 
 
