611ff03f73ec6f2dac93842917d53d704e7d67d4
asmith <asmith151@outlook.com>
2022-01-29 15:05:24 +0000

    Updated make_report. Needs section descriptions

--- capcruncher/pipeline/make_report.py
+++ capcruncher/pipeline/make_report.py
@@ -1,4 +1,5 @@
 import os
+import tempfile
 import pandas as pd
 import numpy as np
 import plotly.express as px
@@ -9,22 +10,21 @@ from plotly.subplots import make_subplots
 def plot_deduplication_stats(deduplication_summary_path: os.PathLike):
 
     df = pd.read_csv(deduplication_summary_path)
-
     fig = px.bar(
         data_frame=df.query('stat_type != "reads_total"'),
         x="stat",
         y="sample",
         color="stat_type",
-        template="simple_white",
+        template="plotly_white",
         category_orders={
             "sample": sorted(df["sample"].unique()),
-            "stat_type": ("reads_unique", "reads_removed"),
+            "stat_type": ["reads_unique", "reads_removed"],
         },
         color_discrete_sequence=["#1f77b4", "grey"],
     )
     fig.for_each_trace(lambda t: t.update(name=" ".join(t.name.split("_"))))
     fig.update_layout(legend_title_text="")
-    fig.update_yaxes(title="Sample")
+    fig.update_yaxes(title="")
     fig.update_xaxes(title="Number of Reads")
     fig.update_traces(marker_line_width=0)
 
@@ -97,26 +97,27 @@ def plot_flash_summary(run_stats_path: os.PathLike):
 
     df = format_run_stats_for_flash_figure(run_stats_path)
     fig = px.bar(
-        data_frame=df,
+        df,
         x="stat",
-        y="stat_type",
+        y="read_type",
         color="read_type",
-        facet_row="sample",
-        template="simple_white",
-        category_orders={
-            "sample": sorted(df["sample"]),
-            "read_type": ["Flashed", "PE"],
-        },
+        animation_frame="sample",
+        range_x=[0, df["stat"].max()],
+        template="plotly_white",
     )
-    fig.update_layout(
-        legend_title_text="",
-        margin={"b": 10},
-    )
-    fig.update_yaxes(title="", autorange="reversed")
-    fig.update_xaxes(matches=None, showticklabels=True)
-    fig.for_each_annotation(lambda a: a.update(text=a.text.split("=")[1]))
-    fig.layout["xaxis"]["title"]["text"] = "Number of Slices (Reads with RE sites)"
-    fig.update_traces(marker_line_width=0)
+
+    fig.update_xaxes(title="Number of Read Pairs")
+    fig.update_yaxes(title="")
+    fig.update_layout(legend_title_text="")
+    try:
+
+        fig["layout"]["updatemenus"][0].update(dict(y=1.2, pad={"b": 10, "t": 0, "l": 0}, x=0))
+        fig["layout"]["sliders"][0]["pad"] = {"b": 10, "t": 25}
+        fig["layout"]["sliders"][0]["x"] = 0
+        fig["layout"]["sliders"][0]["len"] = 1
+
+    except KeyError:  # Might only have one sample
+        pass
 
     return fig
 
@@ -148,7 +149,7 @@ def plot_digestion_read_summary(digestion_stats_reads_path):
         y="stat_type",
         color="read_type",
         facet_row="sample",
-        template="simple_white",
+        template="plotly_white",
         category_orders={
             "sample": sorted(df["sample"]),
             "read_type": ["Flashed", "PE"],
@@ -171,27 +172,40 @@ def plot_digestion_histogram(digestion_stats_histogram_path: os.PathLike):
 
     df = pd.read_csv(digestion_stats_histogram_path)
 
-    fig = px.histogram(
-        data_frame=df.assign(
-            read_number=lambda df: df["read_number"].map(
-                {0: "Flashed", 1: "PE R1", 2: "PE R2"}
-            )
-        ),
-        x="count",
-        y="n_slices",
-        color="read_number",
-        facet_row="sample",
-        template="simple_white",
-        barmode="group",
-        hover_data=["count"],
-        category_orders={"read_number": ["Flashed", "PE R1", "PE R2"]},
+    df["filtered"] = df["filtered"].map({0: "Pre-filtering", 1: "Post-filtering"})
+    df["read_type"] = df["read_type"].map(
+        {"flashed": "Combined Reads", "pe": "Non-Combined Reads"}
     )
 
+    df = df.sort_values(["n_slices", "filtered", "read_type"])
+
+    fig = px.bar(
+        data_frame=df,
+        x="n_slices",
+        y="count",
+        color="filtered",
+        pattern_shape="read_type",
+        animation_frame="sample",
+        animation_group="n_slices",
+        barmode="group",
+        category_orders={"filtered": ["Pre-filtering", "Post-filtering"]},
+        template="plotly_white",
+        range_x=[0, df["n_slices"].max()],
+        range_y=[0, df["count"].max()],
+    )
+
+    fig.update_xaxes(title="")
+    fig.update_yaxes(title="")
     fig.update_layout(legend_title_text="")
-    fig.update_yaxes(title="Frequency", matches=None, showticklabels=True)
-    fig.update_xaxes(dtick=1, showticklabels=True)
-    fig.for_each_annotation(lambda a: a.update(text=a.text.split("=")[1]))
-    fig.update_traces(marker_line_width=0)
+    fig.update_xaxes(dtick=1)
+
+    try:
+        fig["layout"]["updatemenus"][0].update(dict(y=1, pad={"b": 10, "t": 0}, x=0))
+        fig["layout"]["sliders"][0]["pad"] = {"r": 10, "b": 5, "t": 10}
+        fig["layout"]["sliders"][0]["x"] = 0
+        fig["layout"]["sliders"][0]["len"] = 1
+    except KeyError:
+        pass
 
     return fig
 
@@ -215,9 +229,9 @@ def format_alignment_filtering_read_stats(filtering_read_stats_path: os.PathLike
         )
     )
     df.loc[
-        (df["stat_type"] == "Full PCR Duplicate Removal") & 
-        (df["read_type"] == "PE"),
-        "stat",] = (
+        (df["stat_type"] == "Full PCR Duplicate Removal") & (df["read_type"] == "PE"),
+        "stat",
+    ] = (
         df.loc[
             (df["stat_type"] == "Full PCR Duplicate Removal")
             & (df["read_type"] == "PE"),
@@ -231,56 +245,65 @@ def format_alignment_filtering_read_stats(filtering_read_stats_path: os.PathLike
 def plot_alignment_filtering_read_summary(filtering_read_stats_path: os.PathLike):
     df = format_alignment_filtering_read_stats(filtering_read_stats_path)
     fig = px.bar(
-        data_frame=df.sort_values("stat", ascending=False),
+        df,
         x="stat",
         y="stat_type",
-        template="simple_white",
         color="read_type",
-        facet_row="sample",
+        barmode="group",
+        animation_frame="sample",
+        animation_group="stat_type",
+        template="plotly_white",
         category_orders={
             "stat_type": df["stat_type"].unique(),
-            "read_type": ["Flashed", "PE"],
-            "sample": sorted(df["sample"].unique()),
+            "read_type": ["Combined", "Non-Combined"],
         },
+        range_x=[0, df["stat"].max()],
     )
-    fig.update_xaxes(matches=None, showticklabels=True)
+
+    fig.update_xaxes(title="")
     fig.update_yaxes(title="")
-    fig.for_each_annotation(lambda a: a.update(text=a.text.split("=")[1]))
     fig.update_layout(legend_title_text="")
-    fig.update_traces(marker_line_width=0)
+
+    try:
+        fig["layout"]["updatemenus"][0].update(dict(y=1.2, pad={"b": 10, "t": 0}, x=0))
+        fig["layout"]["sliders"][0]["pad"] = {"r": 10, "b": 5, "t": 10}
+        fig["layout"]["sliders"][0]["x"] = 0
+        fig["layout"]["sliders"][0]["len"] = 1
+    except KeyError:
+        pass
 
     return fig
 
 
 def plot_reporter_summary(reporter_stats_path: os.PathLike):
     df = pd.read_csv(reporter_stats_path)
-    n_probes = df["viewpoint"].nunique()
+    df = df.groupby(["sample", "viewpoint", "cis/trans"]).sum().reset_index()
 
     fig = px.bar(
-        data_frame=df.groupby(["sample", "viewpoint", "cis/trans"])
-        .agg({"count": "sum"})
-        .reset_index()
-        .assign(sample=lambda df: df["sample"].str.replace("_", " ")),
-        x="count",
-        y="viewpoint",
+        df.sort_values(["sample", "viewpoint"]),
+        x="viewpoint",
+        y="count",
         color="cis/trans",
-        facet_row="sample",
         barmode="group",
-        template="simple_white",
-        category_orders={
-            "cis/trans": ["trans", "cis"],
-            "viewpoint": sorted(df["viewpoint"].unique()),
-            "sample": sorted(df["sample"].unique()),
-        },
-        labels={"count": "Number of reporters"},
-        facet_row_spacing=0.1,
+        animation_frame="sample",
+        range_y=[0, df["count"].max()],
+        template="plotly_white",
     )
-    fig.update_yaxes(title_text="")
-    fig.update_xaxes(matches=None, showticklabels=True)
-    fig.for_each_trace(lambda t: t.update(name=t.name.split("=")[0]))
-    fig.for_each_annotation(lambda a: a.update(text=a.text.split("=")[1]))
-    fig.update_layout(legend={"traceorder": "reversed", "title": ""})
-    fig.update_traces(marker_line_width=0)
+
+    fig.update_xaxes(title="")
+    fig.update_yaxes(title="")
+    fig.update_layout(legend_title_text="")
+
+    try:
+        fig["layout"]["updatemenus"][0].update(
+            dict(y=1.2, pad={"l": 0, "b": 10, "t": 0}, x=0)
+        )
+        fig["layout"]["sliders"][0]["pad"] = {"r": 0, "b": 5, "t": 50}
+        fig["layout"]["sliders"][0]["x"] = 0
+        fig["layout"]["sliders"][0]["len"] = 1
+    except KeyError:
+        pass
+
     return fig
 
 
@@ -314,64 +337,74 @@ def format_run_stats_for_overall_summary(run_stats_path: os.PathLike):
 
 def plot_overall_summary(run_stats_path: os.PathLike):
 
-    df = format_run_stats_for_overall_summary
+    df = format_run_stats_for_overall_summary(run_stats_path)
 
     fig = px.bar(
         df.query("(read_number != 2) and (stage == stage) "),
         x="stat",
         y="stat_type",
         color="read_type",
-        template="simple_white",
-        facet_row="sample",
+        animation_frame="sample",
+        animation_group="stat_type",
+        barmode="relative",
+        template="plotly_white",
         category_orders={
             "stat_type": df["stat_type"].unique(),
             "sample": sorted(df["sample"].unique()),
             "read_type": ["Flashed", "PE"],
         },
     )
-    fig.update_yaxes(title_text="")
-    fig.update_xaxes(matches=None, showticklabels=True)
-    fig.update_layout(legend_title_text="")
-    fig.for_each_annotation(lambda a: a.update(text=f'{a.text.split("=")[1]}'))
-    fig.layout["xaxis"]["title_text"] = "Number of Read Pairs"
-    fig.update_traces(marker_line_width=0)
+
+    fig.update_yaxes(title="")
+    
+    try:
+        fig["layout"]["updatemenus"][0].update(
+            dict(y=1.2, pad={"l": 0, "b": 5, "t": 0}, x=0)
+        )
+        fig["layout"]["sliders"][0]["pad"] = {"r": 0, "b": 5, "t": 10}
+        fig["layout"]["sliders"][0]["x"] = 0
+        fig["layout"]["sliders"][0]["len"] = 1
+    except KeyError:
+        pass
 
     return fig
 
 
-def plot_report(
-    capcruncher_statistics_path: os.PathLike, output: os.PathLike = "report.html"
+def generate_report(
+    pipeline_statistics_path: os.PathLike,
+    pipeline_report_path: os.PathLike = "report.html",
 ):
 
     # Get paths
     fastq_deduplication_path = os.path.join(
-        capcruncher_statistics_path, "deduplication/deduplication.summary.csv"
+        pipeline_statistics_path, "deduplication/deduplication.summary.csv"
     )
     fastq_trimming_path = os.path.join(
-        capcruncher_statistics_path, "trimming/trimming.summary.csv"
+        pipeline_statistics_path, "trimming/trimming.summary.csv"
     )
     fastq_digestion_hist_path = os.path.join(
-        capcruncher_statistics_path, "digestion/digestion.histogram.csv"
+        pipeline_statistics_path, "digestion/digestion.histogram.csv"
     )
     fastq_digestion_read_path = os.path.join(
-        capcruncher_statistics_path, "digestion/digestion.reads.csv"
+        pipeline_statistics_path, "digestion/digestion.reads.csv"
     )
     reporter_read_path = os.path.join(
-        capcruncher_statistics_path, "reporters/reporters.reads.csv"
+        pipeline_statistics_path, "reporters/reporters.reads.csv"
     )
     reporter_cis_trans_path = os.path.join(
-        capcruncher_statistics_path, "reporters/reporters.reporters.csv"
+        pipeline_statistics_path, "reporters/reporters.reporters.csv"
     )
-    run_stats_path = os.path.join(capcruncher_statistics_path, "run_statistics.csv")
+    run_stats_path = os.path.join(pipeline_statistics_path, "run_statistics.csv")
 
-    # Extract HTML template
-    dir_pipeline = os.path.dirname(os.path.abspath(__file__))
-    path_html_template = os.path.join(dir_pipeline, "report_template.html")
+    # # Extract HTML template
+    # dir_pipeline = os.path.dirname(os.path.abspath(__file__))
+    # path_html_template = os.path.join(dir_pipeline, "report_template.html")
 
     html_header = """
     <html>
     <head>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css">
+    <script src="https://cdn.plot.ly/plotly-latest.min.js"></script>
     <style>
         body {
             margin: 0 100;
@@ -392,8 +425,43 @@ def plot_report(
     <!-- *** Section SECTION_NUMBER *** --->
     <h2>SECTION_NAME</h2>
     <p>SECTION_DESCRIPTION</p>
-    <iframe width="1000" height="1000" frameborder="0" seamless="seamless" scrolling="no" \
-        src="PLOT_URL.embed?width=1000&height=1000"></iframe>"""
+    FIGURE_HTML
+    """
+    # <iframe width="1000" height="1000" frameborder="0" seamless="seamless" scrolling="no" \
+    #src="PLOT_URL.embed?width=1000&height=1000"></iframe>
 
-    # Deduplication
-    plot_deduplication_stats(fastq_deduplication_path)
+
+
+    figures = dict(
+        deduplication=plot_deduplication_stats(
+            fastq_deduplication_path
+        ),  # Deduplication
+        flashed=plot_flash_summary(run_stats_path),  # Flashed
+        digestion_reads=plot_digestion_read_summary(
+            fastq_digestion_read_path
+        ),  # Digestion reads
+        digestion_hist=plot_digestion_histogram(
+            fastq_digestion_hist_path
+        ),  # Digestion histogram
+        alignment_filtering=plot_alignment_filtering_read_summary(
+            reporter_read_path
+        ),  # Filtering
+        reporters=plot_reporter_summary(reporter_cis_trans_path),  # Reporters
+        overall=plot_overall_summary(run_stats_path),  # Overall
+    )
+
+
+    with open(pipeline_report_path, "w") as report:
+
+        report.write(html_header)
+
+        for ii, (fig_name, fig) in enumerate(figures.items()):
+
+            fig_html =  fig.to_html(full_html=False, include_plotlyjs=True if ii == 0 else False, auto_play=False)
+
+
+            report.write(section_template.replace("SECTION_NAME", fig_name)
+                                        .replace("FIGURE_HTML",fig_html))
+                
+        report.write(html_footer)
+
