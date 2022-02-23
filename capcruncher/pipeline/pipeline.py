e3ec729da4cff692d44a0d26c5e079924bb09d1f
alsmith <asmith151@outlook.com>
2022-02-23 09:38:56 +0000

    Replaced job_memory with job_total_memory when appropriate

--- capcruncher/pipeline/pipeline.py
+++ capcruncher/pipeline/pipeline.py
@@ -451,7 +451,7 @@ def fastq_duplicates_parse(infiles, outfile, sample_name, part_no):
         " ".join(statement),
         job_queue=P.PARAMS["pipeline_cluster_queue"],
         job_threads=2,
-        job_memory="6G",
+        job_total_memory="6G",
         job_condaenv=P.PARAMS["conda_env"],
     )
 
@@ -484,7 +484,7 @@ def fastq_duplicates_identify(infiles, outfile):
         " ".join(statement),
         job_queue=P.PARAMS["pipeline_cluster_queue"],
         job_threads=1,
-        job_memory="32G",
+        job_total_memory="32G",
         job_condaenv=P.PARAMS["conda_env"],
     )
 
@@ -1456,7 +1456,7 @@ def alignments_deduplicate_slices(infile, outfile, sample_name, read_type):
         " ".join(statement),
         job_queue=P.PARAMS["pipeline_cluster_queue"],
         job_threads=P.PARAMS["pipeline_n_cores"],
-        job_memory=P.PARAMS["pipeline_memory"],
+        job_total_memory=P.PARAMS["pipeline_memory"],
         job_condaenv=P.PARAMS["conda_env"],
     )
 
