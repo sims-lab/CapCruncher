0a2527ec448676a8b0bc1909d027c0cf3b1e7dc7
alsmith <asmith151@outlook.com>
2023-05-28 15:12:58 +0100

    Added back duplicated reads

--- capcruncher/pipeline/workflow/scripts/combine_deduplication_stats.py
+++ capcruncher/pipeline/workflow/scripts/combine_deduplication_stats.py
@@ -4,8 +4,8 @@ from capcruncher.api.statistics import collate_read_data
 # Read in the data
 df = collate_read_data(snakemake.input)
 
-# Ignore the reads_removed stat_type
-df = df[df["stat_type"] != "reads_removed"]
+# # Ignore the reads_removed stat_type
+# df = df[df["stat_type"] != "reads_removed"]
 
 # Write out the data
 df.to_csv(snakemake.output[0], sep=",", index=False)
