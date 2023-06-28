fd1c3a6bd54c6b1963e94e5d6eca7f9b3dba1da8
alsmith <asmith151@outlook.com>
2023-06-28 15:39:19 +0100

    feat: catching errors for plotting in the log

--- capcruncher/pipeline/workflow/scripts/plot.py
+++ capcruncher/pipeline/workflow/scripts/plot.py
@@ -8,57 +8,60 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 import capcruncher.api.plotting as cp
+from loguru import logger
 
+with logger.catch():
+    # Set-up tracks
+    tracks = []
 
-# Set-up tracks
-tracks = []
+    # Add scale bar
+    tracks.append(cp.CCTrack(None, type="scale"))
 
-# Add scale bar
-tracks.append(cp.CCTrack(None, type="scale"))
+    # Bigwig tracks
+    if snakemake.input.bigwigs:
 
-# Bigwig tracks
-if snakemake.input.bigwigs:
+        for bw in snakemake.input.bigwigs:
+            bw_path = pathlib.Path(bw)
+            tracks.append(cp.CCTrack(bw, type='bigwig', title=bw_path.stem))
+            tracks.append(cp.CCTrack(None, type='spacer'))
 
-    for bw in snakemake.input.bigwigs:
-        bw_path = pathlib.Path(bw)
-        tracks.append(cp.CCTrack(bw, type='bigwig', title=bw_path.stem))
-        tracks.append(cp.CCTrack(None, type='spacer'))
-
-# Add subtractions if available
-if snakemake.input.subtractions:
-    for sub in snakemake.input.subtractions:
-        sub_path = pathlib.Path(sub)
-        tracks.append(cp.CCTrack(sub, type='bigwig', title=sub_path.stem, min="auto"))
-        tracks.append(cp.CCTrack(None, type='spacer'))
-
-# Add heatmaps if available
-if snakemake.input.heatmaps:
-    for hm in snakemake.input.heatmaps:
-        hm_path = pathlib.Path(hm)
-        tracks.append(
-            cp.CCTrack(
-                hm,
-                type='heatmap',
-                title=hm_path.stem,
-                binsize=snakemake.params.binsize,
-                viewpoint=snakemake.params.viewpoint,
+    # Add subtractions if available
+    if snakemake.input.subtractions:
+        for sub in snakemake.input.subtractions:
+            sub_path = pathlib.Path(sub)
+            tracks.append(
+                cp.CCTrack(sub, type='bigwig', title=sub_path.stem, min="auto")
             )
-        )
+            tracks.append(cp.CCTrack(None, type='spacer'))
+
+    # Add heatmaps if available
+    if snakemake.input.heatmaps:
+        for hm in snakemake.input.heatmaps:
+            hm_path = pathlib.Path(hm)
+            tracks.append(
+                cp.CCTrack(
+                    hm,
+                    type='heatmap',
+                    title=hm_path.stem,
+                    binsize=snakemake.params.binsize,
+                    viewpoint=snakemake.params.viewpoint,
+                )
+            )
+            tracks.append(cp.CCTrack(None, type='spacer'))
+
+    # Add genes if available
+    if snakemake.params.genes:
+        genes = snakemake.params.genes
+        genes_path = pathlib.Path(genes)
+        tracks.append(cp.CCTrack(genes, type='genes'))
         tracks.append(cp.CCTrack(None, type='spacer'))
 
-# Add genes if available
-if snakemake.params.genes:
-    genes = snakemake.params.genes
-    genes_path = pathlib.Path(genes)
-    tracks.append(cp.CCTrack(genes, type='genes'))
-    tracks.append(cp.CCTrack(None, type='spacer'))
+    # Add X-axis
+    tracks.append(cp.CCTrack(None, type='xaxis'))
 
-# Add X-axis
-tracks.append(cp.CCTrack(None, type='xaxis'))
+    # Make figure and save
+    fig = cp.CCFigure(tracks)
+    fig.save(snakemake.params.coordinates, output=snakemake.output.fig)
 
-# Make figure and save
-fig = cp.CCFigure(tracks)
-fig.save(snakemake.params.coordinates, output=snakemake.output.fig)
-
-# Export template used to make figure
-fig.to_toml(snakemake.output.template)
+    # Export template used to make figure
+    fig.to_toml(snakemake.output.template)
