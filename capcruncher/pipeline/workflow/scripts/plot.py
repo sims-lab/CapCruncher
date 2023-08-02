88fe5ea9a31130e21c56ba9e319f05b7178a63ad
alsmith <asmith151@outlook.com>
2023-08-02 13:02:29 +0100

    chore: `type` changed to `file_type` for clarity

--- capcruncher/pipeline/workflow/scripts/plot.py
+++ capcruncher/pipeline/workflow/scripts/plot.py
@@ -17,24 +17,24 @@ with logger.catch():
     tracks = []
 
     # Add scale bar
-    tracks.append(cp.CCTrack(None, type="scale"))
+    tracks.append(cp.CCTrack(None, file_type="scale"))
 
     # Bigwig tracks
     if snakemake.input.bigwigs:
 
         for bw in snakemake.input.bigwigs:
             bw_path = pathlib.Path(bw)
-            tracks.append(cp.CCTrack(bw, type='bigwig', title=bw_path.stem))
-            tracks.append(cp.CCTrack(None, type='spacer'))
+            tracks.append(cp.CCTrack(bw, file_type='bigwig', title=bw_path.stem))
+            tracks.append(cp.CCTrack(None, file_type='spacer'))
 
     # Add subtractions if available
     if snakemake.input.subtractions:
         for sub in snakemake.input.subtractions:
             sub_path = pathlib.Path(sub)
             tracks.append(
-                cp.CCTrack(sub, type='bigwig', title=sub_path.stem, min="auto")
+                cp.CCTrack(sub, file_type='bigwig', title=sub_path.stem, min="auto")
             )
-            tracks.append(cp.CCTrack(None, type='spacer'))
+            tracks.append(cp.CCTrack(None, file_type='spacer'))
 
     # Add heatmaps if available
     if snakemake.input.heatmaps:
@@ -43,24 +43,24 @@ with logger.catch():
             tracks.append(
                 cp.CCTrack(
                     hm,
-                    type='heatmap',
+                    file_type='heatmap',
                     title=hm_path.stem,
                     binsize=snakemake.params.binsize,
                     viewpoint=snakemake.params.viewpoint,
                     style="triangular",
                 )
             )
-            tracks.append(cp.CCTrack(None, type='spacer'))
+            tracks.append(cp.CCTrack(None, file_type='spacer'))
 
     # Add genes if available
     if snakemake.params.genes:
         genes = snakemake.params.genes
         genes_path = pathlib.Path(genes)
-        tracks.append(cp.CCTrack(genes, type='genes'))
-        tracks.append(cp.CCTrack(None, type='spacer'))
+        tracks.append(cp.CCTrack(genes, file_type='genes'))
+        tracks.append(cp.CCTrack(None, file_type='spacer'))
 
     # Add X-axis
-    tracks.append(cp.CCTrack(None, type='xaxis'))
+    tracks.append(cp.CCTrack(None, file_type='xaxis'))
 
     # Make figure and save
     fig = cp.CCFigure(tracks)
