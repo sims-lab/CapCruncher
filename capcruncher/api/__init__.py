29a49f7295599b07d1ff3ab6eac3cfad6bcea78a
alsmith151 <alastair.smith@ndcls.ox.ac.uk>
2025-02-07 16:47:04 +0000

    refactor(api): reorganize __all__ exports for improved readability

--- capcruncher/api/__init__.py
+++ capcruncher/api/__init__.py
@@ -1,1 +1,19 @@
-__all__ = ["annotate", "count", "deduplicate", "digest", "filter", "io", "pileup", "plotting", "statistics", "storage"]
+from . import annotate
+from . import deduplicate
+from . import filter
+from . import io
+from . import pileup
+from . import plotting
+from . import statistics
+from . import storage
+
+__all__ = [
+    "annotate",
+    "deduplicate",
+    "filter",
+    "io",
+    "pileup",
+    "plotting",
+    "statistics",
+    "storage",
+]
