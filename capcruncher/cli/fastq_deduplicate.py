4d924882c2506a832de56d61e7a07bcf17f24088
alsmith151 <alastair.smith@ndcls.ox.ac.uk>
2026-04-26 23:07:59 +0100

    refactor: merge fastq cli implementations

--- capcruncher/cli/fastq_deduplicate.py
+++ capcruncher/cli/fastq_deduplicate.py
@@ -1,50 +1,5 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Fri Oct  4 13:47:20 2019
-@author: asmith
-"""
-from typing import List, Union
-from loguru import logger as logging
-import pathlib
-from capcruncher.api.statistics import FastqDeduplicationStatistics
-from capcruncher_tools.api import deduplicate_fastq
+"""Compatibility imports for FASTQ deduplication."""
 
+from capcruncher.cli.cli_fastq import deduplicate_fastq as deduplicate
 
-
-def deduplicate(
-    fastq_1: List[str],
-    fastq_2: List[str],
-    output_prefix: Union[str, pathlib.Path] = "deduplicated_",
-    statistics: str = "deduplication_statistics.json",
-    sample_name: str = "sampleX",
-    shuffle: bool = False,
-    **kwargs,
-):
-
-
-    df_stats = deduplicate_fastq(
-        fastq1=fastq_1,
-        fastq2=fastq_2,
-        output_prefix=output_prefix,
-        sample_name=sample_name,
-        shuffle=shuffle,
-    )
-    
-    dedup_stats = FastqDeduplicationStatistics(
-        sample=sample_name,
-        total=df_stats.query("stat_type == 'read_pairs_total'")["stat"].values[0],
-        duplicates=df_stats.query("stat_type == 'read_pairs_duplicated'")["stat"].values[0],
-    )
-    with open(statistics, "w") as f:
-        f.write(dedup_stats.model_dump_json())
-
-    
-
-    logging.info("Printing deduplication statistics to stdout")
-    # Print stats to stdout
-    df_vis = df_stats.copy()
-    df_vis["stat_type"] = df_vis["stat_type"].str.replace("_", " ").str.title()
-    df_vis = df_vis[["stat_type", "stat"]]
-    df_vis.columns = ["Stat Type", "Number of Reads"]
-    print(df_vis.to_string(index=False))
+__all__ = ["deduplicate"]
--- capcruncher/cli/fastq_digest.py
+++ capcruncher/cli/fastq_digest.py
@@ -1,55 +1,5 @@
-import os
-from typing import Literal, Tuple, Dict
+"""Compatibility imports for FASTQ digestion."""
 
-import pandas as pd
-from loguru import logger as logging
-import polars as pl
+from capcruncher.cli.cli_fastq import digest_fastq as digest
 
-
-def digest(
-    fastqs: Tuple,
-    restriction_site: str,
-    mode: Literal["flashed", "pe"] = "pe",
-    output_file: os.PathLike = "out.fastq.gz",
-    minimum_slice_length: int = 18,
-    statistics: os.PathLike = "digest.json",
-    sample_name: str = "sampleX",
-    **kwargs,
-) -> Dict[str, pl.DataFrame]:
-    """
-    Digest FASTQ files.
-
-    Args:
-        fastqs: Tuple of FASTQ files.
-        restriction_site: Restriction enzyme name or sequence to use for in silico digestion.
-        mode: Digestion mode. Combined (Flashed) or non-combined (PE) read pairs.
-        output_file: Output file path.
-        minimum_slice_length: Minimum slice length.
-        statstics: Output prefix for stats file.
-        sample_name: Name of sample e.g. DOX_treated_1. Required for correct statistics.
-
-    Returns:
-        A dictionary of stats: stats_read_level, stats_hist_unfilt, stats_hist_filt
-    """
-    from capcruncher_tools.api import digest_fastq
-    from capcruncher.utils import get_restriction_site
-
-    logging.info("Digesting FASTQ files")
-
-    if len(fastqs) > 1 and mode == "flashed":
-        raise ValueError("Flashed mode can only be used with a single FASTQ file")
-
-    stats = digest_fastq(
-        fastqs=fastqs,
-        restriction_site=get_restriction_site(restriction_site),
-        output=output_file,
-        read_type=mode.title(),
-        sample_name=sample_name,
-        minimum_slice_length=minimum_slice_length,
-    )
-
-    logging.info("Digestion complete. Generating statistics")
-    with open(statistics, "w") as f:
-        f.write(stats.model_dump_json())
-
-    return stats
+__all__ = ["digest"]
